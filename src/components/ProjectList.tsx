fdf4df0a04e8eb2567072f5cf2263d6f6d76cb7b
RenanEduardo <Ecommercerenan@gmail.com>
2025-05-23 01:23:41 -0300

    feat: implementação dos componentes para listar e criar os projetos.

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,12 @@
+import {  useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+type Project = {
+    id: number;
+    name: string;
+    description: string;
+};
+
+export default function ProjectList() {
+    
+}
